0918190058015d79cd56c62505c15e00b06fe59b
Wiktor <wiktor.projekty.konto@gmail.com>
2025-08-26 12:18:41 +0200

    User display JS update v1

--- djangoproject/a20_users/urls.py
+++ djangoproject/a20_users/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from a20_users.views import ViewLogedUserSettings, ViewUserWeek_overview, ViewUserCurrentDay_modify, ViewAdminAllHours_overview
-from a20_users.views import ViewAdminAllHours_overview, ViewAdminAllHours_modify, ViewAdminAllHours_delete, view_user_cooperation_get
-urlpatterns = [
-    path(route= "user_connections_overview/", view= view_user_cooperation_get, name= "user_connections_overview"),
-    
-    path(route= "week_overview_js/", view=ViewUserWeek_overview.as_view(), name= "week_overview_js"),
-    path(route= "xday_modify/<int:id>/<str:cd>", view= ViewUserCurrentDay_modify.as_view(), name= "xday_modify"),
-    # path(route= "xday_modify/<int:id>", view= ViewUserCurrentDay_modify.as_view(), name= "xday_modify"),
-    path(route= "xday_delete/<int:id>", view= ViewAdminAllHours_delete.as_view(), name= "xday_delete"),
+from a20_users.views import ViewAdminAllHours_overview, ViewAdminAllHours_modify, ViewAdminAllHours_delete
+from a20_users.views import ViewUserWeek_overview, ViewUserCurrentDay_delete ,ViewAdminAllHours_overview
+from a20_users.views import ViewUserCurrentDay_modify, ViewUserCurrentDay_delete, ViewUserCurrentDay_overwiev, view_user_cooperation_get
 
-    path(route= "all_hours_overview/", view=ViewAdminAllHours_overview.as_view(), name= "all_hours_overview"),
-    path(route= "all_hours_modify/<int:id>", view=ViewAdminAllHours_modify.as_view(), name= "all_hours_modify"),
+urlpatterns = [
+    path(route= "week_overview_grouby/",       view= ViewUserWeek_overview.as_view(),  name= "week_overview_grouby"),
+    path(route= "xday_overview/<str:cd>",  view= ViewUserCurrentDay_overwiev.as_view(),name= "xday_overview"),
+    path(route= "xday_modify/<int:id>/<str:cd>",view= ViewUserCurrentDay_modify.as_view(),  name= "xday_modify"),
+    path(route= "xday_delete/<int:id>/<str:cd>",view= ViewUserCurrentDay_delete.as_view(),  name= "xday_delete"),
+
+    path(route= "all_hours_overview/",      view=ViewAdminAllHours_overview.as_view(),name= "all_hours_overview"),
+    path(route= "all_hours_modify/<int:id>",view=ViewAdminAllHours_modify.as_view(), name= "all_hours_modify"),
+    
+    path(route= "user_connections_overview/",view= view_user_cooperation_get, name= "user_connections_overview"),
 ]
--- djangoproject/a20_users/views.py
+++ djangoproject/a20_users/views.py
@@ -100,8 +100,8 @@ def view_user_cooperation_get(request:HttpRequest):#{
 #**********************************************************************************************************************
 # 
 class ViewUserWeek_overview(View):
-    HTML_FILE = "week_overview_js.html"
-    URL_LINK = "week_overview_js"
+    HTML_FILE = "week_overview_grouby_v2.html"
+    URL_LINK = "week_overview_grouby"
     
     @staticmethod
     def _create_isostring_list(datestart:date, days:int)->list[str]:#{
@@ -120,18 +120,19 @@ class ViewUserWeek_overview(View):
         iso_date_list = self._create_isostring_list(date_start, days_amount)
 
         weekhours = UsersWorkTimes.objects.filter(user= request.user).filter(date__gte= date_start).filter(date__lte= date_end).values(
-            "id","date","travel_start", "travel_end", "work_start", "work_end", "workimes_standard__description", "project_machine", "netzplan_vorgang")
+            "date","travel_start", "travel_end", "work_start", "work_end", "workimes_standard__description", "project_machine", "netzplan_vorgang")
         json_weekhours = list(weekhours)
         #json_weekhours = [model_to_dict(i) for i in  weekhours]
+        link_dir = "/a20_users/xday_overview/"
 
-        context = {"iso_date_list" :  iso_date_list, "settings" : settings, "json_weekhours" : json_weekhours}
+        context = {"iso_date_list": iso_date_list, "settings": settings, "json_weekhours": json_weekhours, "json_link_dir": link_dir}
         return render(request= request, template_name= self.HTML_FILE, context= context)
     #}
     def post(self, request:HttpRequest):#{
         date_now = request.POST.get("date_now", "")
         days_amount = request.POST.get("days_amount", "")
         
-        if date_now:
+        if date_now:#[
             settings = {}
             settings["days_amount"] = int(days_amount)
 
@@ -147,8 +148,68 @@ class ViewUserWeek_overview(View):
                 request.session["VIEW_SETTINGS"] = {}
                 _save_view_settings(settings, request.session["VIEW_SETTINGS"])
             request.session.modified = True
-
+        #]
         return HttpResponseRedirect(reverse(self.URL_LINK))
+    #}
+#======================================================================================================================
+
+#**********************************************************************************************************************
+#  Query set for specyfic day
+class ViewUserCurrentDay_overwiev(View):
+    HTML_FILE = "xday_overview.html"
+    URL_LINK = "xday_overview"
+
+    def get(self, request:HttpRequest, cd:str):#{
+        current_entries = request.session.get("LAST_ENTRY", {})
+
+        form = FormWorktimeEntry(current_entries) 
+        current_date = date.fromisoformat(cd)
+
+        query_set = UsersWorkTimes.objects.filter(user= request.user).filter(date= current_date)
+        context = {"query_set" : query_set, "form" : form, "id" : id, "cd" : cd}
+        return render(request= request, template_name= self.HTML_FILE, context= context)
+    #}
+    def post(self, request:HttpRequest, cd:str):#{
+        current_date = date.fromisoformat(cd)
+
+        form = FormWorktimeEntry(data= request.POST)
+        if form.is_valid():#[
+            current_entries = {}
+
+            for key in ["travel_start", "travel_end", "work_start", "work_end"]:
+                val = form.cleaned_data[key]
+                current_entries[key] = val.isoformat() if val else ""
+            _save_last_input(current_entries, request, "LAST_ENTRY")
+            
+            # (currententry, ifCreated) = UsersWorkTimes.objects.get_or_create(user= request.user, date= current_date)
+            new_user_work_times = UsersWorkTimes.objects.create(user= request.user, date= current_date)
+            if form.cleaned_data["travel_start"]:
+                new_user_work_times.travel_start = form.cleaned_data["travel_start"]
+
+            if form.cleaned_data["travel_end"]:
+                new_user_work_times.travel_end = form.cleaned_data["travel_end"]
+
+            if form.cleaned_data["work_start"]:
+                new_user_work_times.work_start = form.cleaned_data["work_start"]
+
+            if form.cleaned_data["work_end"]:
+                new_user_work_times.work_end = form.cleaned_data["work_end"]
+
+            if form.cleaned_data["worktimes_standard"]:
+                new_user_work_times.workimes_standard = form.cleaned_data["worktimes_standard"]
+
+            if form.cleaned_data["project_machine"]:
+                new_user_work_times.project_machine = str(form.cleaned_data["project_machine"])
+
+            if form.cleaned_data["netzplan_vorgang"]:
+                new_user_work_times.netzplan_vorgang = str(form.cleaned_data["netzplan_vorgang"])
+
+            new_user_work_times.save()
+            return HttpResponseRedirect(reverse(self.URL_LINK, args=[cd]))
+        #]
+        else:#[
+            error = f"Input form error! cleaned_data= {form.changed_data}"
+            return render(request= request, template_name= A20_ERROR_HTML, context= {"error" : error, "form" : form})
     #}
 #======================================================================================================================
 
@@ -159,57 +220,92 @@ class ViewUserCurrentDay_modify(View):
     URL_LINK = "xday_modify"
 
     def get(self, request:HttpRequest, id:int, cd:str):#{
+        try:
+            user_work_time = UsersWorkTimes.objects.get(pk= int(id))
+        except UsersWorkTimes.DoesNotExist:
+            error = f"Current user work entry does not exist! id={id}"
+            return render(request= request, template_name= A20_ERROR_HTML, context= {"error" : error})
+        
         current_entries = request.session.get("LAST_ENTRY", {})
-
         form = FormWorktimeEntry(current_entries) 
-        current_date = date.fromisoformat(cd)
 
-        query_set = UsersWorkTimes.objects.filter(user= request.user).filter(date= current_date)
-        context = {"currententry" : query_set, "form" : form, "id" : id, "cd" : cd}
+        context = {"uwt" : user_work_time, "form" : form, "cd" : cd}
         return render(request= request, template_name= self.HTML_FILE, context= context)
     #}
-
     def post(self, request:HttpRequest, id:int, cd:str):#{
         current_date = date.fromisoformat(cd)
 
         form = FormWorktimeEntry(data= request.POST)
-        if form.is_valid():#[]
+        if form.is_valid():#[
             current_entries = {}
 
-            for key in ["travel_start", "travel_end", "work_start", "work_end"]:
+            for key in ["date","travel_start", "travel_end", "work_start", "work_end"]:
                 val = form.cleaned_data[key]
                 current_entries[key] = val.isoformat() if val else ""
             _save_last_input(current_entries, request, "LAST_ENTRY")
             
-            (currententry, ifCreated) = UsersWorkTimes.objects.get_or_create(user= request.user, date= current_date)
+            user_work_time = UsersWorkTimes.objects.get(pk= id)
             if form.cleaned_data["travel_start"]:
-                currententry.travel_start = form.cleaned_data["travel_start"]
+                user_work_time.travel_start = form.cleaned_data["travel_start"]
 
             if form.cleaned_data["travel_end"]:
-                currententry.travel_end = form.cleaned_data["travel_end"]
+                user_work_time.travel_end = form.cleaned_data["travel_end"]
 
             if form.cleaned_data["work_start"]:
-                currententry.work_start = form.cleaned_data["work_start"]
+                user_work_time.work_start = form.cleaned_data["work_start"]
 
             if form.cleaned_data["work_end"]:
-                currententry.work_end = form.cleaned_data["work_end"]
+                user_work_time.work_end = form.cleaned_data["work_end"]
 
             if form.cleaned_data["worktimes_standard"]:
-                currententry.workimes_standard = form.cleaned_data["worktimes_standard"]
+                user_work_time.workimes_standard = form.cleaned_data["worktimes_standard"]
 
             if form.cleaned_data["project_machine"]:
-                currententry.project_machine = str(form.cleaned_data["project_machine"])
+                user_work_time.project_machine = str(form.cleaned_data["project_machine"])
 
             if form.cleaned_data["netzplan_vorgang"]:
-                currententry.netzplan_vorgang = str(form.cleaned_data["netzplan_vorgang"])
+                user_work_time.netzplan_vorgang = str(form.cleaned_data["netzplan_vorgang"])
 
-            currententry.save()
-            return HttpResponseRedirect(reverse(self.URL_LINK, args=[id, cd]))
+            user_work_time.save()
+            return HttpResponseRedirect(reverse(ViewUserCurrentDay_overwiev.URL_LINK, args=[cd]))
         #]
         else:#[
             error = f"Input form error! cleaned_data= {form.changed_data}"
             return render(request= request, template_name= A20_ERROR_HTML, context= {"error" : error, "form" : form})
     #}
+#======================================================================================================================
+
+#**********************************************************************************************************************
+# Administrator user hours overview with posibolity to add new and change
+class ViewUserCurrentDay_delete(View):
+    HTML_FILE = "xday_delete.html"
+    
+    def get(self, request:HttpRequest, id:int, cd:str):#{
+        try:
+            user_work_time = UsersWorkTimes.objects.get(pk= int(id))
+        except UsersWorkTimes.DoesNotExist:#{
+            error = f"Current user work entry does not exist! id={id}"
+            return render(request= request, template_name= A20_ERROR_HTML, context= {"error" : error})
+        #}
+        current_entries = request.session.get("LAST_ENTRY", {})
+        form = FormWorktimeEntry(current_entries) 
+
+        context = {"currententry" : user_work_time, "form" : form, "cd" : cd}
+        return render(request= request, template_name= self.HTML_FILE, context= context)
+    #}
+    def post(self, request:HttpRequest, id:int, cd:str):#{
+        operation = request.POST.get("operation")
+        
+        if operation == "delete":
+            user_work_time = UsersWorkTimes.objects.get(pk= int(id))
+            user_work_time.delete()
+            return HttpResponseRedirect(reverse(ViewUserCurrentDay_overwiev.URL_LINK, args=[cd]))
+        elif operation == "cancel":
+            return HttpResponseRedirect(reverse(ViewUserCurrentDay_overwiev.URL_LINK, args=[cd]))
+        else:
+            error = f"Input form error, not delete, not cancel!"
+            return render(request= request, template_name= A20_ERROR_HTML, context= {"error" : error})
+        #}
 #======================================================================================================================
 
 #**********************************************************************************************************************
